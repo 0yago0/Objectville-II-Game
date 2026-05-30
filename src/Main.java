52613d9384dbd12ec71b27d00996b7c425667046
Asrinada <adakolbasi@gmail.com>
2026-05-30 19:38:54 +0300

    Added Main class with command line arguments

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
+import io.MapReader;
+import model.City;
+import model.MapConfigException;
+
+// Main entry point of the simulation, handles command line arguments and initialization
 public class Main {
+
+    public static void main(String[] args) throws MapConfigException {
+        if (args.length != 2) {
+            throw new MapConfigException("Invalid command line arguments. Expected: <map_file> <ticks>");
+        }
+
+        String fileName = args[0];
+        int ticks;
+
+        try {
+            ticks = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            throw new MapConfigException("Tick count must be a valid integer number.");
+        }
+
+        MapReader mapReader = new MapReader();
+        City city = mapReader.readMap(fileName);
+    }
 }
